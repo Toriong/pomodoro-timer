d34a9427576e4b8dcb28813fd1d9c250c2032879
Gabe <gtorion97@gmail.com>
2022-05-23 21:15:42 -0500

    css of navbar created

---
+++ src/PomodoroTimer.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './css/index.css';
+import Navbar from './components/navbar/Navbar';
+
+const PomodoroTimer = () => {
+  return (
+    <div className="pomodoroTimerPage">
+      <Navbar />
+    </div>
+  );
+}
+
+export default PomodoroTimer;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import '../../css/navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='unfixed-wrapper'>
+            <div className='navbar'>
+                <div className='navbarSubContainer'>
+                    <section>
+                        <h1>Gabe's Pomodoro timer</h1>
+                    </section>
+                    <section>
+                        <button>
+                            <GiHamburgerMenu />
+                            <span>Settings</span>
+                        </button>
+                    </section>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Navbar;
